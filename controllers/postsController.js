5f79de9cccb8cd0ff66f4c358e14186e4d228887
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-12 17:33:33 +0100

    controller delete

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -15,4 +15,17 @@ const getPostById = (req, res) => {
   res.json(post);
 };
 
+const deletePost = (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = posts.findIndex((post) => post.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "Post non trovato" });
+  }
+
+  posts.splice(index, 1);
+  console.log("Lista aggiornata:", posts);
+  res.status(204).send();
+};
+
 module.exports = { getAllPosts, getPostById, deletePost };
