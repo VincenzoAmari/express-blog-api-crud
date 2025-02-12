d0e4dc3efdc33f85d59adff66501933f86a5f271
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-12 17:29:13 +0100

    controller show by id

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,4 +4,15 @@ const getAllPosts = (req, res) => {
   res.json(posts);
 };
 
+const getPostById = (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({ error: "Post non trovato" });
+  }
+
+  res.json(post);
+};
+
 module.exports = { getAllPosts, getPostById, deletePost };
