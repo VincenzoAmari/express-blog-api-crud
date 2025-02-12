ca4e79a682ec6f58190f06886393d2bfa1825fbe
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-12 17:20:25 +0100

    messo prima le rotte ora creo le condizioni

--- routers/posts.js
+++ routers/posts.js
@@ -1,36 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getAllPosts,
+  getPostById,
+  deletePost,
+} = require("../controllers/postsController");
 
-// Rotta per ottenere i post (index)
-router.get("/", (req, res) => {
-  res.send("Lista dei post");
+// Rotta per ottenere tutti i post
+router.get("/", getAllPosts);
 
-  res.json(post);
-});
+// Rotta per ottenere un post tramite id
+router.get("/:id", getPostById);
 
-// Rotta per ottenere un singolo post (show)
-router.get("/:id", (req, res) => {
-  res.send(`Dettaglio del post ${req.params.id}`);
-});
+// // Rotta per creare un nuovo post (create)
+// router.post("/", (req, res) => {
+//   res.send("Creazione di un nuovo post");
+// });
 
-// Rotta per creare un nuovo post (create)
-router.post("/", (req, res) => {
-  res.send("Creazione di un nuovo post");
-});
+// // Rotta per aggiornare un post esistente (update)
+// router.put("/:id", (req, res) => {
+//   res.send(`Aggiornamento del post ${req.params.id}`);
+// });
 
-// Rotta per aggiornare un post esistente (update)
-router.put("/:id", (req, res) => {
-  res.send(`Aggiornamento del post ${req.params.id}`);
-});
-
-// Rotta per aggiornare parzialmente un post (patch)
-router.patch("/:id", (req, res) => {
-  res.send(`Aggiornamento parziale del post ${req.params.id}`);
-});
+// // Rotta per aggiornare parzialmente un post (patch)
+// router.patch("/:id", (req, res) => {
+//   res.send(`Aggiornamento parziale del post ${req.params.id}`);
+// });
 
 // Rotta per cancellare un post (delete)
-router.delete("/:id", (req, res) => {
-  res.send(`Cancellazione del post ${req.params.id}`);
-});
+router.delete("/:id", deletePost);
 
 module.exports = router;
