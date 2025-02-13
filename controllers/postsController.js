7d264f33e08b6611ca42999adb9e57fc7ec7300e
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-13 16:05:04 +0100

    update controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -46,6 +46,24 @@ const createPost = (req, res) => {
   res.status(201).json(newPost);
 };
 
+//Aggiorna post
+const updatePost = (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id);
+
+  if (!post) {
+    return res.status(404).json({ error: "Post non trovato" });
+  }
+
+  const { titolo, contenuto, immagine, tags } = req.body;
+  if (titolo) post.titolo = titolo;
+  if (contenuto) post.contenuto = contenuto;
+  if (immagine) post.immagine = immagine;
+  if (tags) post.tags = tags;
+
+  res.json(post);
+};
+
 //cancella post con id selezionato
 const deletePost = (req, res) => {
   const id = parseInt(req.params.id);
